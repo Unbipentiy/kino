a9a74f385cf7fcacd3f83bd1252e68b8799a0fa9
Antuant <laptew.26@yandex.ru>
2021-02-22 21:25:06 +0700

    1.42
    qwe

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', '\App\Http\Controllers\Controller@home')->name('home');
+Route::get('/', '\App\Http\Controllers\PageController@home')->name('home');
