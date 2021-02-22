43022c3a31e500e1c95d74c508f8982947565b7e
Antuant <laptew.26@yandex.ru>
2021-02-22 21:40:59 +0700

    1.44
    rout'ы

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
+    public function registration(Request $request){
+        User::create([
+            'email' => $request->email,
+            'password' => $request->password,
+            'name' => $request->name,
+        ]);
+
+    }
+    public function viewRegistration(){
+        return view('registration');
+    }
+
+    public function login(){
+
+    }
+
+    public function viewLogin(){
+        return view('login');
+    }
 }
