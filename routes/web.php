05c28a851bf2e85692dcbc0da5222290ced8edfc
Antuant <laptew.26@yandex.ru>
2021-02-22 21:50:08 +0700

    1.46
    routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', '\App\Http\Controllers\PageController@home')->name('home');
 
 Route::get('/registration', '\App\Http\Controllers\AuthController@viewRegistration')->name('registration');
+Route::post('/registration', '\App\Http\Controllers\AuthController@registration')->name('registration_post');
 Route::get('/login', '\App\Http\Controllers\AuthController@login')->name('login');
 
 Route::get('/admin', '\App\Http\Controllers\PageController@admin')->name('admin');
