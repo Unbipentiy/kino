74dabae0d3822fb3e5eedfb088f4c5821e4d728b
Andrew <super.rain-gain@yandex.ru>
2021-02-22 21:41:37 +0700

    1.44

---
+++ resources/views/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li>1 lnk</li>
+            <li>2 lnk</li>
+            <li>3 lnk</li>
+            <li>4 lnk</li>
+        </ul>
+    </nav>
+</header>
